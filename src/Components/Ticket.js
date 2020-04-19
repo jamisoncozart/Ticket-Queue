51e39de6150aa39fecc3a49265edee507ba95b27
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-19 08:42:07 -0700

    Add props to Tickets

--- src/Components/Ticket.js
+++ src/Components/Ticket.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-function Ticket() {
+function Ticket(props) {
   const name='Jamison';
   const name2='Nichole';
   return(
     <React.Fragment>
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
       <hr />
     </React.Fragment>
   );
--- src/Components/TicketList.js
+++ src/Components/TicketList.js
@@ -3,7 +3,14 @@ import Ticket from './Ticket';
 
 function TicketList() {
   return (
-    <Ticket />
+    <Ticket 
+      location='3A'
+      names="Jamison and Nichole"
+      issue="Firebase will not save record!" />
+    <Ticket 
+      location='4B'
+      names="Pat and Jim"
+      issue="Prop types are throwing an error" />
   );
 }
 
